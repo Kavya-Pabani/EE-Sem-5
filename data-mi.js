89c1b8b7d7589a058ccdb7d5de7c150784337c90
Kavya-Pabani <kavyapabani@gmail.com>
2026-07-18 14:15:08 +0530

    Create data-mi.js

---
+++ data-mi.js
@@ -0,0 +1,21 @@
+const miData = {
+  subjectName: "Measurement and Instrumentation",
+  chapters: [
+    {
+      chapterName: "Chapter 1: Measuring Instruments",
+      topics: [
+        {
+          topicName: "PMMC Instruments",
+          quizText: `
+            Q: Permanent Magnet Moving Coil (PMMC) instruments can be used to measure which type of current?
+            A) DC only
+            B) AC only
+            C) Both AC and DC
+            D) Pulsating AC only
+            Correct: A
+          `
+        }
+      ]
+    }
+  ]
+};
