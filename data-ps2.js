5db017ffd2ef89f40df6a7e62053f4f1725cbf06
Kavya-Pabani <kavyapabani@gmail.com>
2026-07-18 14:13:41 +0530

    Create data-ps2.js

---
+++ data-ps2.js
@@ -0,0 +1,28 @@
+const ps2Data = {
+  subjectName: "Power Systems II",
+  chapters: [
+    {
+      chapterName: "Chapter 1: Line Performance",
+      topics: [
+        {
+          topicName: "Short Transmission Lines",
+          quizText: `
+            Q: Which parameter is completely neglected in short transmission lines?
+            A) Resistance
+            B) Inductance
+            C) Capacitance
+            D) Impedance
+            Correct: C
+
+            Q: The length of a typical short transmission line is usually less than:
+            A) 20 km
+            B) 80 km
+            C) 150 km
+            D) 300 km
+            Correct: B
+          `
+        }
+      ]
+    }
+  ]
+};
