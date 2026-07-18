7d5cf7f217743711de62f6174598584a809f5fb7
Kavya-Pabani <kavyapabani@gmail.com>
2026-07-18 14:14:26 +0530

    Create data-eds.js

---
+++ data-eds.js
@@ -0,0 +1,21 @@
+const edsData = {
+  subjectName: "Electrical Distribution System",
+  chapters: [
+    {
+      chapterName: "Chapter 1: Distribution Basics",
+      topics: [
+        {
+          topicName: "Feeder Design Criteria",
+          quizText: `
+            Q: A feeder in a distribution system is designed primarily based on which criteria?
+            A) Current carrying capacity
+            B) Voltage drop
+            C) Power factor
+            D) Frequency stability
+            Correct: A
+          `
+        }
+      ]
+    }
+  ]
+};
