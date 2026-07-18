2f8ff666b2b3a5e5101cb58bf473444818b50ba5
Kavya-Pabani <kavyapabani@gmail.com>
2026-07-18 14:16:37 +0530

    Create data-cs.js

---
+++ data-cs.js
@@ -0,0 +1,21 @@
+const csData = {
+  subjectName: "Control Systems",
+  chapters: [
+    {
+      chapterName: "Chapter 1: Time Response Analysis",
+      topics: [
+        {
+          topicName: "Steady State Error",
+          quizText: `
+            Q: A type-0 system subjected to a step input results in a steady-state error that is:
+            A) Zero
+            B) Infinite
+            C) Constant finite value
+            D) Undefined
+            Correct: C
+          `
+        }
+      ]
+    }
+  ]
+};
