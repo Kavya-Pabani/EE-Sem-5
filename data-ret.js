c49801bcb2862cbce2eb3d6089682b829c000d8f
Kavya-Pabani <kavyapabani@gmail.com>
2026-07-18 14:16:00 +0530

    Create data-ret.js

---
+++ data-ret.js
@@ -0,0 +1,21 @@
+const retData = {
+  subjectName: "Renewable Energy Technology",
+  chapters: [
+    {
+      chapterName: "Chapter 1: Solar Energy",
+      topics: [
+        {
+          topicName: "Photovoltaic Cell Basics",
+          quizText: `
+            Q: What is the primary semiconductor material used in commercial solar PV cells?
+            A) Germanium
+            B) Silicon
+            C) Gallium Arsenide
+            D) Copper
+            Correct: B
+          `
+        }
+      ]
+    }
+  ]
+};
